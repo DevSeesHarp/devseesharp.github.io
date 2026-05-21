2ef8dbcaebe03c34bf308ab1e7e8c66ff0e12919
sebastian lindblom <sebastian.lindblom@eq2.se>
2026-05-22 01:19:05 +0200

    Renaming Blazorwasm project to blazorwasm and moving it to src folder

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Eq2.Application.Web.Blazorwasm;
+using blazorwasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
